dc0126db47809f32e04784d21de645b1b19671ac
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-03 01:05:26 -0400

    Build Outline of Game function
    First attempta t building a function that will loop rounds until player
    or computer achieve a score of 5

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function roundWin(){
+    playerScore += 1;
+    return `You Win! ${playerSelection} beats ${computerSelection}.`;
+}
+function roundLoss(){
+    computerScore +=1;
+    return `You Lose :(! ${playerSelection} beats ${computerSelection}.`
+}
+function roundDraw(){
+    return `It's a draw!!!`;
+}
 
 function getComputerChoice() {
     let possibleChoices = ['Rock', 'Paper', 'Scissors'];
@@ -13,47 +24,38 @@ function firstLetterCap(string) {
     return normalString;
 }
 
-// function roundWin(){
-//     return `You Win! ${playerSelection} beats ${computerSelection}.`;
-// }
-
-// function roundLoss(){
-//     return `You Lose :(! ${playerSelection} beats ${computerSelection}.`
-// }
-
-// function roundDraw(){
-//     return `It's a draw!!!`;
-// }
-function roundWin(){
-    return `You Win! ${playerSelection} beats ${computerSelection}.`;
-}
-function roundLoss(){
-    return `You Lose :(! ${playerSelection} beats ${computerSelection}.`
-}
-function roundDraw(){
-    return `It's a draw!!!`;
-}
-
 function playRound(playerSelection, computerSelection){
     playerSelection = firstLetterCap(playerSelection);
     if (playerSelection === computerSelection){
         return roundDraw();
     } else if (playerSelection === 'Rock'){
         if (computerSelection === 'Scissors'){
-           return roundWin();
+        return roundWin();
         } else return roundLoss();
     } else if (playerSelection === 'Paper'){
         if (computerSelection === 'Rock'){
-           return roundWin();
+        return roundWin();
         } else return roundLoss();
     } else if (playerSelection === 'Scissors'){
         if (computerSelection === 'Paper'){
-            return roundWin();
+        return roundWin();
         } else return roundLoss();
     }
 }
 
-f
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let computerSelection = getComputerChoice();
+let playerSelection = prompt('Rock, Paper or Scissors? ;D')
+let playerScore = 0;
+let computerScore = 0;
+
+function game(){
+    while (playerScore < 5 || computerScore < 5){
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Current Score is ${playerScore} - ${computerScore}`)
+    }
+    if (playerScore = 5) {
+        return `Victory!!!!`
+    } else return 'L Bozo :(, try again by refreshing '
+}
+
+console.log(game())
