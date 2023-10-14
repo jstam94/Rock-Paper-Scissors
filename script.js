3e2fb903ace9526e6b889677eb0344a20d75a8b7
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-14 16:10:37 -0400

    Refactor Code
    Made changes to make code cleaner and function scope more logical
    in preperation of input being provided by DOM events.gi

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function roundWin(){
-    playerScore += 1;
-    return `You Win! ${playerSelection} beats ${computerSelection}.`;
-}
-function roundLoss(){
-    computerScore +=1;
-    return `You Lose :(! ${computerSelection} beats ${playerSelection}.`
-}
-function roundDraw(){
-    return `It's a draw!!!`;
-}
+function playRound(playerSelection){
+    let computerRoll = () =>{
+        let possibleChoices = ['Rock', 'Paper', 'Scissors'];
+        let computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+        return computerChoice;
+    }
 
-function getComputerChoice() {
-    let possibleChoices = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
-    return computerChoice;
-}
-
-function firstLetterCap(string) {
-    string = string.toLowerCase();
-    let firstLetter = (string.charAt(0)).toUpperCase();
-    let endOfWord =  string.slice(1, string.length)
-    let normalString = firstLetter + endOfWord
-    return normalString;
-}
-
-function playRound(playerSelection, computerSelection){
-    playerSelection = firstLetterCap(playerSelection);
+    let computerSelection = computerRoll();
+    
+    roundWin = () => {
+        playerScore += 1;
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    roundLoss = () => {
+        computerScore +=1;
+        return `You Lose :(! ${computerSelection} beats ${playerSelection}.`
+     }
+    roundDraw = () =>{
+        return `It's a draw!!!`;
+     }
+    
     if (playerSelection === computerSelection){
         return roundDraw();
     } else if (playerSelection === 'Rock'){
@@ -42,43 +35,10 @@ function playRound(playerSelection, computerSelection){
     } else if (playerSelection === 'Scissors'){
         if (computerSelection === 'Paper'){
         return roundWin();
-        } else return roundLoss();
+        } else {
+             return roundLoss();}
+        }
     }
-}
 
-function game(){
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Score: ${playerScore} - ${computerScore}`);
 
-playerSelection = firstLetterCap(prompt('Round Two xD'));
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Score: ${playerScore} - ${computerScore}`);
-
-playerSelection = firstLetterCap(prompt('Round Three'));
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Score: ${playerScore} - ${computerScore}`);
-
-playerSelection = firstLetterCap(prompt('Round Four'));
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Score: ${playerScore} - ${computerScore}`);
-
-playerSelection = firstLetterCap(prompt('Last Round!!!!!!'));
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Score: ${playerScore} - ${computerScore}`);
-
-if (playerScore === computerScore){
-    console.log('Final Result: Draw...')
-} else if (playerScore > computerScore){
-    console.log('Final Result: Victory!!!!')
-} else if (computerScore > playerScore){
-    console.log(`Final Result: You Lose. NT NT GGs :(`)
-}
-}
-
-let playerSelection = firstLetterCap(prompt('Rock, Paper, Scissors'));
-let computerSelection = getComputerChoice();
-game()
+console.log(playRound('Scissors'));
